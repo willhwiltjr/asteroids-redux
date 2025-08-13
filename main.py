286fc828a27450a7443a5fe140702f6dd9ae1570
willhwiltjr <wiltwijr@gmail.com>
2025-08-13 13:39:24 -0700

    C: started the player controller our triangle rotates

--- main.py
+++ main.py
@@ -13,15 +13,21 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock=pygame.time.Clock()
     player=Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
     black =(0,0,0,255)
+    dt=0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(black)
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
+        dt=clock.tick(60)/1000
+
 
 
 if __name__ == "__main__":
--- player.py
+++ player.py
@@ -1,4 +1,6 @@
 from circleshape import *
+from constants import *
+
 
 
 class Player(CircleShape):
@@ -9,6 +11,18 @@ class Player(CircleShape):
     def draw(self, screen):
         white = (255,255,255)
         pygame.draw.polygon(screen,white,self.triangle(),2)
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-1*dt)
+            # ?
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+
+            # ?
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED*dt
 
 # in the player class
     def triangle(self):
