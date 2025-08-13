0f74febbf7f17decee38e789f139fcffe1fe0d4e
willhwiltjr <wiltwijr@gmail.com>
2025-08-13 10:43:33 -0700

    A:  initial commit setup pywindow and basic game loop

---
+++ main.py
@@ -0,0 +1,23 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+from constants import *
+import pygame
+pygame.init()
+
+def main():
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    black =(0,0,0,255)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(black)
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
