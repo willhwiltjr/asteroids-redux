6470d9263373ef27540eca88ffc6b7dbea3561ae
willhwiltjr <wiltwijr@gmail.com>
2025-08-14 07:44:49 -0700

    J:added ashoot timer now its only one bullet

--- player.py
+++ player.py
@@ -9,6 +9,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation=0
+        self.shot_timer=0
 
     def draw(self, screen):
         pygame.draw.polygon(screen,"white",self.triangle(),2)
@@ -23,6 +24,7 @@ class Player(CircleShape):
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        self.shot_timer-=dt
 
         if keys[pygame.K_a]:
             self.rotate(-1*dt)
@@ -38,12 +40,15 @@ class Player(CircleShape):
             self.move(dt)
 
         if keys[pygame.K_SPACE]:
-            self.shoot()
+            if self.shot_timer>0:
+                print("cant shoot")
+            else:
+                self.shoot()
 
     def shoot(self):
-        print("Shot created")
         shot = Shot(self.position.x, self.position.y)
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        self.shot_timer=PLAYER_SHOOT_COOLDOWN
 
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED*dt
